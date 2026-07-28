1d8ae48055fbbae96b5f7025025a164d43f3ac26
Hung Nguyen <honghung.dotdak@gmail.com>
2026-07-28 22:26:46 +0700

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        moves = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        def dfs(grid, r, c, visited):
+            if min(r, c) < 0 or r >= ROWS or c >= COLS or grid[r][c] == '0' or (r, c) in visited:
+                return 0
+            # if r == ROWS - 1 and c == COLS - 1:
+            #     return grid[r][c] == '1'
+            visited.add((r,c))
+            for dr, dc in moves:
+                dfs(grid, r + dr, c + dc, visited)
+            return 1
+
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                count += dfs(grid, r, c, visited)
+
+        return count
+
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        moves = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        def dfs(r, c): 
+            if not 0 <= r < ROWS or not 0 <= c < COLS:
+                return False
+            if (r, c) in visited or grid[r][c] == '0':
+                return False
+            visited.add((r, c))
+            for dr, dc in moves:
+                dfs(r + dr, c + dc)
+
+            return True
+
+        ans = 0
+        for r in ROWS:
+            for c in COLS:
+                if dfs((r, c)):
+                    ans += 1
+        return ans
+        
+        
